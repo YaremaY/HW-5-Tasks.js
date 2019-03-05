0f6ee8a3a60f9fa166ad8abf9fa622ac280d1f0b
Yarema <47628249+YaremaY@users.noreply.github.com>
2019-03-05 15:53:19 +0200

    Update Tasks.js

--- Tasks.js
+++ Tasks.js
@@ -1,1 +1,25 @@
+1.................................................................................................................................
 
+<!DOCTYPE html>
+<html>
+<title>CurrencyConverter</title>
+<body>
+
+<h2>CurrencyConverter</h2>
+<p>Function that does currency converter from USD to UAH:</p>
+
+<p>
+  <label>USD</label>
+  <input id="inputUSD" type="number" placeholder="USD" oninput="CurrencyConverter(this.value)" onchange="CurrencyConverter(this.value)">
+</p>
+<p>UAH: <span id="outputUAH"></span></p>
+
+<script>
+function CurrencyConverter(valNum) {
+  document.getElementById("outputUAH").innerHTML=valNum * 8;
+}
+</script>
+</body>
+</html>
+
+2................................................................................................................................
